01c8bcd20cf93fa875d8b062ca5e92426e30190e
tniemeye19 <timothy.niemeyer19@gmail.com>
2022-01-09 21:26:07 -0500

    implement adding items to the cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -6,7 +6,7 @@ import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 
 import { useStoreContext } from '../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../utils/actions';
+import { UPDATE_PRODUCTS, REMOVE_FROM_CART, UPDATE_CART_QUANTITY, ADD_TO_CART } from '../utils/actions';
 
 import Cart from '../components/Cart';
 
@@ -30,6 +30,13 @@ function Detail() {
       });
     }
   }, [products, data, dispatch, id]);
+  
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...currentProduct, purchaseQuantity: 1 }
+    });
+  };
 
   return (
     <>
@@ -43,7 +50,7 @@ function Detail() {
 
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
+            <button onClick={addToCart}>Add to Cart</button>
             <button>Remove from Cart</button>
           </p>
 
